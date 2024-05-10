040258c72c10844a8ba26f88b4e7aba2b16e319e
本田珠彩 <ayame@kataba.me>
2024-05-10 11:36:00 +0000

    Update: [misc] update package name 📄

--- gist.sh
+++ gist.sh
@@ -38,7 +38,7 @@ else
         gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev \
         libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev \
         libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev \
-        vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 \
+        vulkan-utility-libraries-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 \
         libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev \
         libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev \
         libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev \
