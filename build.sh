d842fb3fe4ba6680d11fcac87f8eba0d963e4318
本田珠彩 <ayame@kataba.me>
2024-07-14 16:43:03 +0000

    Revert: "Fix: [lib] do not build xcb-errors"
    This reverts commit 082c17be2a62cca5c56dca3d25fe5e897613088b.

--- build.sh
+++ build.sh
@@ -31,7 +31,8 @@ cd ~/hyprsource
 
 
 ## 01 basic build tools
-echo "::group::Install basic dependencies"
+echo "::group::"
+echo "Install basic dependencies"
 ensure_root apt-get update
 apt_install \
     ca-certificates meson jq cmake-extras \
@@ -42,7 +43,8 @@ echo "::endgroup::"
 # cf. https://gist.github.com/Vertecedoc4545/6e54487f07a1888b656b656c0cdd9764
 # cf. https://gist.github.com/katabame/e368988c968278c83c19bd5f5b60f407
 # those should be sorted (which package required by what)
-echo "::group::Install additional dependencies"
+echo "::group::"
+echo "Install additional dependencies"
 apt_install \
     gettext gettext-base fontconfig libfontconfig-dev \
     libxkbcommon-x11-dev libxkbregistry-dev seatd libvulkan-dev \
@@ -54,7 +56,8 @@ echo "::endgroup::"
 # 03 determine repository tag
 case "$1" in
     nightly)
-        echo "::group::Build target: nightly"
+        echo "::group::"
+        echo "Build target: nightly"
         HYPRCURSOR_TAG='main'
         HYPRLAND_PROTOCOLS_TAG='main'
         HYPRLAND_TAG='main'
@@ -65,10 +68,10 @@ case "$1" in
         WAYLAND_PROTOCOLS_TAG='main'
         WAYLAND_TAG='main'
         XCB_ERRORS_TAG='master'
-        HYPRPAPER_TAG='main'
     ;;
     canary)
-        echo "::group::Build target: canary"
+        echo "::group::"
+        echo "Build target: canary"
         HYPRCURSOR_TAG=`curl https://api.github.com/repos/hyprwm/hyprcursor/releases/latest | jq -r '.tag_name'`
         HYPRLAND_PROTOCOLS_TAG=`curl https://api.github.com/repos/hyprwm/hyprland-protocols/releases/latest | jq -r '.tag_name'`
         HYPRLAND_TAG=`curl https://api.github.com/repos/hyprwm/hyprland/releases/latest | jq -r '.tag_name'`
@@ -79,10 +82,10 @@ case "$1" in
         WAYLAND_PROTOCOLS_TAG=`curl https://gitlab.freedesktop.org/api/v4/projects/2891/repository/tags | jq -r '.[0].name'`
         WAYLAND_TAG=`curl https://gitlab.freedesktop.org/api/v4/projects/121/repository/tags | jq -r '.[0].name'`
         XCB_ERRORS_TAG=`curl https://gitlab.freedesktop.org/api/v4/projects/2433/repository/tags | jq -r '.[0].name'`
-        HYPRPAPER_TAG=`curl https://api.github.com/repos/hyprwm/hyprpaper/releases/latest | jq -r '.tag_name'`
     ;;
     *)
-        echo "::group::Build target: stable"
+        echo "::group::"
+        echo "Build target: stable"
         # Build successful versions as of 2024-06-28 02:00 AM GMT+9
         HYPRCURSOR_TAG='v0.1.9'                  # https://github.com/hyprwm/hyprcursor/releases
         HYPRLAND_PROTOCOLS_TAG='v0.3.0'          # https://github.com/hyprwm/hyprland-protocols/releases
@@ -94,7 +97,6 @@ case "$1" in
         WAYLAND_PROTOCOLS_TAG='1.36'             # https://gitlab.freedesktop.org/wayland/wayland-protocols/-/tags
         WAYLAND_TAG='1.23.0'                     # https://gitlab.freedesktop.org/wayland/wayland/-/tags
         XCB_ERRORS_TAG='xcb-util-errors-1.0.1'   # https://gitlab.freedesktop.org/xorg/lib/libxcb-errors/-/tags
-        HYPRPAPER_TAG='v0.7.0'                   # https://github.com/hyprwm/hyprpaper/releases
     ;;
 esac
 echo "### 📦 Build details" # >> $GITHUB_STEP_SUMMARY
@@ -113,15 +115,15 @@ echo "|xorg/lib/libxcb-errors|[${XCB_ERRORS_TAG}](https://gitlab.freedesktop.org
 echo "::endgroup::"
 
 # 70 libxcb-errors
-#echo "::group::Build libxcb-errors"
-#git clone --depth 1 --branch ${XCB_ERRORS_TAG} --recurse-submodules https://gitlab.freedesktop.org/xorg/lib/libxcb-errors.git
-#cd ./libxcb-errors
-#    apt_install xutils-dev libtool xcb-proto
-#    ./autogen.sh
-#    ./configure
-#    ensure_root make install
-#cd ~/hyprsource
-#echo "::endgroup::"
+echo "::group::Build libxcb-errors"
+git clone --depth 1 --branch ${XCB_ERRORS_TAG} --recurse-submodules https://gitlab.freedesktop.org/xorg/lib/libxcb-errors.git
+cd ./libxcb-errors
+    apt_install xutils-dev libtool xcb-proto
+    ./autogen.sh
+    ./configure
+    ensure_root make install
+cd ~/hyprsource
+echo "::endgroup::"
 
 # 90 hyprutils
 echo "::group::Build hyprutils"
@@ -230,16 +232,5 @@ cd ./hyprland
     cp ./hyprpm/src/hyprpm ~/hyprsource/artifact
     cp ./src/Hyprland ~/hyprsource/artifact
     ensure_root ninja install
-cd ~
-echo "::endgroup::"
-
-# 100 tweak hyprland commands
-echo "::group::Tweak hyprland commands"
-git clone --depth 1 --branch ${HYPRPAPER_TAG} https://github.com/hyprwm/hyprpaper.git
-cd ./hyprpaper
-    apt_install libxcb-util-dev libmagic-dev
-    cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
-    cmake --build ./build --config Release --target hyprpaper -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
-    ensure_root cmake --install ./build
 cd ~
 echo "::endgroup::"
